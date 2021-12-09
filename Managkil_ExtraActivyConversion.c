c556431ba1c927fea64645fe9ff94a30ab26dbbe
T86-Managkil <89286454+T86-Managkil@users.noreply.github.com>
2021-12-09 12:18:41 +0800

    Add files via upload

---
+++ Managkil_ExtraActivyConversion.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+int main(){
+    
+    char input[100],cur;
+    int len;
+    printf("Input any numbers: ");
+    scanf("%s", input);
+    len = strlen(input);
+    printf("The results are: ", input);
+    
+    for(int x = 0;x<len;x++){
+        cur = input[x];
+        if(cur=='0'){
+            printf("Zero ");
+        }
+        else if(cur == '1'){
+            printf("One ");
+        }
+        else if(cur == '2'){
+            printf("Two ");
+        }
+        else if(cur == '3'){
+            printf("Three ");
+        }
+        else if(cur == '4'){
+            printf("Four ");
+        }
+        else if(cur == '5'){
+            printf("Five ");
+        }
+        else if(cur == '6'){
+            printf("Six ");
+        }
+        else if(cur == '7'){
+            printf("Seven ");
+        }
+        else if(cur == '8'){
+            printf("Eight ");
+        }
+        else if(cur == '9'){
+            printf("Nine ");
+        }
+        else{
+            printf("Invalid Input! ");
+        }
+    }
+    getch();
+    return 0;
+}
